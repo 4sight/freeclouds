47b9d42c8ddd7df7dd2540bc3033e7e1b8d4ea6d
Greeley Dawson <hgd4th@gmail.com>
2014-08-15 18:28:39 -0400

    added home view

---
+++ app/scripts/connect.js
@@ -0,0 +1,12 @@
+var Connect = Backbone.View.extend({
+  el: 'authenticate',
+
+  initialize: function (){
+    $('span#welcome').hide();
+    this.render();
+    },
+
+  render: function(){
+    $('span#welcome').hide();
+    this.render();
+  });
