f0aa48c3f090de4313ef82666dab5d7a87e36f3c
Greeley Dawson <hgd4th@gmail.com>
2014-08-15 09:44:35 -0400

    accessing user feeds

--- app/scripts/stream.js
+++ app/scripts/stream.js
@@ -29,19 +29,45 @@ var Stream = Backbone.View.extend({
 
     render: function(){
       SC.initialize({ client_id: setting.clientId });
-      SC.get("/tracks", {limit: 20}, function(tracks){
+      $.ajax({
+        url: resourceHost + '/me',
+        data: {
+          client_id: setting.clientId
+        },
+        beforeSend: function (xhr) {
+          xhr.setRequestHeader('Authorization', "OAuth " + token);
+          xhr.setRequestHeader('Accept',        "application/json");
+        },
+        success: function (response) {
+            var nothing;
+            return response.username;
+        }
+      }); 
+      SC.get('/me/activities?oauth_token=' + token, {limit: 30}, function(tracks){
+        console.log(token);
         var track = tracks;
-        _.each(_.range(0,20), function(num){
-          SC.oEmbed(track[num].uri, {}, function (oembed) {
-            $('.hero-unit').append(oembed.html);
+        _.each(_.range(0,30), function(num){
+          console.log(num);
+          console.log(track.collection);
+          SC.oEmbed(track.collection[num].origin.uri, {}, function (oembed) {
+            $('.hero-unit').append('<div id="sound' + num + '">' + oembed.html + '</div>');
+            // var str0 = $('#sound' + num).html();
+            // var str1 = $(str0).contents();
+            // var str2 = str1.prevObject[0].outerHTML;
+            // console.log(str2);
+            // var iframe = document.getElementsByTagName('iframe');
+            // console.log(iframe.contentDocument);
+            // var innerDoc = (iframe.contentDocument) ? iframe.contentDocument : iframe.contentWindow.document;
+            // .find(".sc-button-group");
           });
-          // var text = $('div#sound' + num).html();
+          // var str = $('#sound' + num).html();
+          // console.log(str);
           // var match = text.search('sc-button-download');
           // console.log(match);
           // if (match = -1){
           //   console.log(num);
           // } else {
-          //   $('div#sound' + num).removeClass('hidden');
+          //   $('#sound' + num).removeClass('hidden');
           // }
         }
       );
