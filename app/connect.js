b1a9c27b0478b1e369ec1eb8950cbf59866d91d3
4sight <hgd4th@gmail.com>
2017-09-13 21:22:31 -0400

    rollback to first master commit on 2017.09.13 with added connect.js

---
+++ app/connect.js
@@ -0,0 +1,20 @@
+var Connect = Backbone.View.extend({
+  el: 'body',
+
+  initialize: function (){
+    this.render();
+    },
+
+  render: function(){
+    $('#wrapper').hide();
+    $('span#welcome').hide();
+    $('span#number').hide();
+    $('div.authenticate').mouseenter(function(){
+      $('div.authenticate').addClass('connectHover');
+    });
+    $('div.authenticate').mouseleave(function(){
+      $('div.authenticate').removeClass('connectHover');
+      }
+    );
+  }
+});
