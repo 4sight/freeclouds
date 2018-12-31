53bf2b2cb43e4280f951fb8fbbb2372de864ee95
4sight <hgd4th@gmail.com>
2018-12-31 01:02:48 -0500

    working genre filter

--- app/scripts/stream.js
+++ app/scripts/stream.js
@@ -74,32 +74,39 @@ var Stream = Backbone.View.extend({
     });
     console.log(genres);
     function search(){
-      var searchArray = [];
-      var regex = new RegExp(genre, 'ig');
-      var genre = document.getElementById('genreInput').value;
       SC.get('/me/activities?oauth_token=' + token, {limit: 200}, function(tracks){
-        var number = document.getElementById('number');
+        console.log(tracks);
         var examined = 0;
         var shown = 0;
-        while (shown < number.value){
-          if (searchArray[i] != -1 || null || undefined){
-            var i;
-            for (i = 0; i <= tracks.collection.length; i++){
-              if (tracks.collection[i] != null || undefined){
-              searchArray[i] = tracks.collection[i].origin.genre.search(regex)};
-              SC.oEmbed(tracks.collection[i].origin.uri, {}, function(oembed){
-                $('#wrapper').append('<div class="sound">' + oembed.html + '</div>');
-              });
-              shown++;
-            };
-            examined++;
-          } else {
-            searchArray[i] = -1;
-          };
-        };
-      });
-      $('#wrapper').empty();
+        var number = document.getElementById('number');
+        while (shown < number.value && examined < tracks.collection.length){            
+          if (tracks.collection[examined].origin){
+            var genres = [];
+            genres[examined] = tracks.collection[examined].origin.genre;
+            var genreArray = [];
+            var genre = document.getElementById('genreInput').value;
+            var regex = new RegExp(genre, 'ig');
+            genreArray[examined] = genres[examined].search(regex);
+            console.log(genreArray);
+            genreArray.forEach(function(){
+              if (tracks.collection[examined].origin && genreArray[examined] != -1){
+                SC.oEmbed(tracks.collection[examined].origin.uri, {}, function(oembed){
+                  $('#wrapper').append('<div class="sound">' + oembed.html + '</div>');
+                });
+              };
+            });
+          shown++;
+        }
+        examined++;
+      }
+      console.log(examined + ' tracks scanned');
+    });
+    $('#wrapper').empty();
     };
     document.getElementById('searchButton').addEventListener('click', search);
   }
 });
+// if (searchArray[i] != -1 || null || undefined){
+// var i;
+// for (i = 0; i <= tracks.collection.length; i++){
+// if (tracks.collection[i] != null || undefined){
