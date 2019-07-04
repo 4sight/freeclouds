4928059de33e09906e36b7f3d477e6e8dc718f52
4sight <hgd4th@gmail.com>
2019-07-03 20:43:58 -0400

    work

--- app/scripts/stream.js
+++ app/scripts/stream.js
@@ -31,10 +31,10 @@ var Stream = Backbone.View.extend({
     $('div#home').hide();
     $('div#homeHalo').hide();
     $('.home').removeClass();
-    if($(window).width() > 950){
+    if ($(window).width() > 950){
       $('#breakForSubmitButton').remove();
     };
-    if($(window).width() > 950){
+    if ($(window).width() > 950){
       $('breakForGenreSearchButton').remove();
     };
     SC.initialize({ client_id: setting.clientId });
@@ -54,7 +54,12 @@ var Stream = Backbone.View.extend({
       }
     });
     var genres = [];
-    SC.get('/me/activities?oauth_token=' + token, {limit: 200}, function(tracks){
+    var genreArray = [];
+    var shown = 0;
+    SC.get('/me/activities?oauth_token=' + token, {limit: 200}, function(tracks, error){
+      if (error) {
+          window.alert('Soundcloud is temporarily denying freecloud&#39;s request for data. Please try again later.');
+      } else {
       console.log(tracks);
       var number = document.getElementById('number');
       var i;
@@ -67,7 +72,6 @@ var Stream = Backbone.View.extend({
         };
       };
       var examined = 0;
-      var shown = 0;
       while (shown < number.value && examined < tracks.collection.length){
         if (tracks.collection[examined].origin){
           $('#wrapper').append('<div class="sound"><iframe width=\"100%\" height=\"400\" scrolling=\"no\" frameborder=\"no\" src="https://w.soundcloud.com/player/?visual=true&url=' + tracks.collection[examined].origin.uri + '"</div>');
@@ -76,15 +80,17 @@ var Stream = Backbone.View.extend({
         examined++;
       }
       console.log(examined + ' tracks scanned');
-    });
+    }});
     console.log(genres);
-    function search(){
+    function searchFunction(){
       $('#wrapper').empty();
-      var genreArray = [];
       // If search field is not sumbitted blank...
       if (document.getElementById('genreInput').value != ''){
         // Get up to 200 tracks from the users' Soundcloud feed
-        SC.get('/me/activities?oauth_token=' + token, {limit: 200}, function(tracks){
+        SC.get('/me/activities?oauth_token=' + token, {limit: 200}, function(tracks, error){
+          if (error) {
+            window.alert('Soundcloud is temporarily denying freecloud&#39;s request for data. Please try again later.');
+        } else { 
           var examined = 0;
           var shown = 0;
           var start = Date.now();
@@ -102,7 +108,6 @@ var Stream = Backbone.View.extend({
                 if (genreArray[examined] != -1){
                   $('#wrapper').append('<div class="sound"><iframe width=\"100%\" height=\"400\" scrolling=\"no\" frameborder=\"no\" src="https://w.soundcloud.com/player/?visual=true&url=' + tracks.collection[examined].origin.uri + '"</div>');
                   shown++;
-                  console.log(genreArray);
                 } else {
                   genres[examined] = -1;
                 };
@@ -113,14 +118,16 @@ var Stream = Backbone.View.extend({
               }
             }
           console.log(examined + ' tracks scanned');
-        });
+        }});
       } else {
         // If the default stream is displayed, a blank search does nothing.
         if (genreArray.length > 0){}
         // If search results are displayed, a blank search resets the window.
         else {
           $('#wrapper').empty();
-          SC.get('/me/activities?oauth_token=' + token, {limit: 200}, function(tracks){
+          SC.get('/me/activities?oauth_token=' + token, {limit: 200}, function(tracks, error){
+            if (error) {
+              window.alert('Soundcloud is temporarily denying freecloud&#39;s request for data. Please try again later.');
             console.log(tracks);
             var number = document.getElementById('number');
             var i;
@@ -141,15 +148,18 @@ var Stream = Backbone.View.extend({
               examined++;
             }
             console.log(examined + ' tracks scanned');
-          });
+          }});
         };
       };
     };
-    document.getElementById('searchButton').addEventListener('click', search);
+    document.getElementById('searchButton').addEventListener('click', searchFunction);
     // Reset button
     function reset(){
       $('#wrapper').empty();
-      SC.get('/me/activities?oauth_token=' + token, {limit: 200}, function(tracks){
+      SC.get('/me/activities?oauth_token=' + token, {limit: 200}, function(tracks, error){
+        if (error) {
+          window.alert('Soundcloud is temporarily denying freecloud&#39;s request for data. Please try again later.');
+        } else {
         console.log(tracks);
         var number = document.getElementById('number');
         var i;
@@ -163,36 +173,55 @@ var Stream = Backbone.View.extend({
           examined++;
         }
         console.log(examined + ' tracks scanned');
-      });
+      }});
     };
-    document.getElementById('resetButton').addEventListener('click', reset);
-
+  document.getElementById('resetButton').addEventListener('click', reset);
   $('#submitButton').click(function(){
-    SC.get('/me/activities?oauth_token=' + token, {limit: 200}, function(tracks){
-      $('#wrapper').empty();
-      console.log(tracks);
-      var number = document.getElementById('number').value;
-      console.log(number);
-      var i;
-      for (i = 0; i <= tracks.collection.length; i++){
-        if (tracks.collection[i] == null || tracks.collection[i].origin == null){
-          genres[i] = -1;
-        } else {
-          // Put genres into array
-          genres[i] = tracks.collection[i].origin.genre;
-        };
-      };
-      var examined = 0;
-      var shown = 0;
-      while (shown < number && examined < tracks.collection.length){
-        if (tracks.collection[examined].origin){
-          $('#wrapper').append('<div class="sound"><iframe width=\"100%\" height=\"400\" scrolling=\"no\" frameborder=\"no\" src="https://w.soundcloud.com/player/?visual=true&url=' + tracks.collection[examined].origin.uri + '"</div>');
-          shown++;
-        }
-        examined++;
+    var newNumber = document.getElementById('number').value;
+    if (newNumber === shown){console.log('no change')}
+    else if (newNumber < shown){
+      for (i = 0; i < (shown - newNumber); i++){
+        $('#wrapper').children('div[class=sound]:last').remove();
+        console.log('remove ' + i);
       }
-      console.log(examined + ' tracks scanned');
-      });
-  });
+    }
+    else {
+      // Get up to 200 tracks from the users' Soundcloud feed
+      SC.get('/me/activities?oauth_token=' + token, {limit: 200}, function(tracks, error){
+        if (error) {
+          window.alert('Soundcloud is temporarily denying freecloud&#39;s request for data. Please try again later.');
+        } else {
+          $('#wrapper').empty();
+          var examined = 0;
+          var shown = 0;
+          var start = Date.now();
+          // This line specifies how many tracks to show:
+          while (shown < newNumber && examined < tracks.collection.length && (Date.now() - start < 5000)){        
+            // If the examined track has some metadata (is not "null")...
+            if (tracks.collection[examined].origin){
+              var genre = document.getElementById('genreInput').value;
+              var regex = new RegExp(genre, 'ig');
+              // ...and if the track has a genre...
+              if (genres[examined] != null){
+                console.log(examined);
+                // ...put the regex search results into genreArray.
+                genreArray[examined] = genres[examined].toString().search(regex);
+                if (genreArray[examined] != -1){
+                  $('#wrapper').append('<div class="sound"><iframe width=\"100%\" height=\"400\" scrolling=\"no\" frameborder=\"no\" src="https://w.soundcloud.com/player/?visual=true&url=' + tracks.collection[examined].origin.uri + '"</div>');
+                  shown++;
+                } else {
+                  genreArray[examined] = -1;
+                };
+              
+              examined++;
+              } else {
+              genreArray[examined] = -1;
+            }}
+          }
+          console.log(examined + ' tracks scanned');
+          console.log(genreArray);
+      }});
+      };
+    });
   }
 });
